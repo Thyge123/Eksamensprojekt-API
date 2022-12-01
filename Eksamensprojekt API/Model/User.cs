64bb54e8c12a5702d62c38a7dce76de1c9d6857b
Thyge123 <martintsh25@gmail.com>
2022-12-01 09:17:44 +0100

    dfg

--- Eksamensprojekt API/Model/User.cs
+++ Eksamensprojekt API/Model/User.cs
@@ -6,8 +6,8 @@
         public string Name { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
-        public int? ZipCode { get; set; }
-        public int? phoneNumber { get; set; }
+        public int ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
 
