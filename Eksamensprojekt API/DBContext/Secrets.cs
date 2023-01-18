928f5cd02f99608abe275a19346f304bf258bb01
Thyge123 <martintsh25@gmail.com>
2023-01-18 10:41:43 +0100

    sdf

--- Eksamensprojekt API/DBContext/Secrets.cs
+++ Eksamensprojekt API/DBContext/Secrets.cs
@@ -2,6 +2,6 @@
 {
     public class Secrets
     {
-        public static readonly string connectionString = "Server=tcp:thygedatabase.database.windows.net,1433;Initial Catalog=TrashCans;Persist Security Info=False;User ID=Thyge;Password=Boohmanden12;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static readonly string connectionString = "Server=tcp:thygedatabase.database.windows.net,1433;Initial Catalog=ThygeDatabase;Persist Security Info=False;User ID=Thyge;Password=Boohmanden12;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
